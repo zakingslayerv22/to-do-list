1bbfb25acb7db473d0c3f771d20ab37e2c53cd1a
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2024-08-31 06:47:07 +0200

    Refactor createTask() to Task
    Convert the createTask() to
    a Task class so that methods
    to update the values of the
    object properties can be
    added to the Task class.
    Exporting the Task class also
    exports these methods. It
    is better than exporting
    individual functions.
    
    Add the status parameter to
    the constructor so that the
    task can be set to completed
    or pending. Add the addTask()
    as a method in the Task class.
    Add other methods will be
    used to edit the values of
    the properties of the
    objects created with the
    Task class.
    
    Update task1 to include a
    value for the status
    property and call
    the addTask method to
    push task1 into
    the tasks array of the first
    element in the project array.

--- src/tasks-module/tasks.module.js
+++ src/tasks-module/tasks.module.js
@@ -2,19 +2,52 @@
 
 import { projectsArray } from "../projects-module/project.module";
 
+export class Task {
+    constructor (title, description, deadline, priority, status) {
+        this.title = title;
+        this.description = description;
+        this.deadline = deadline;
+        this.priority = priority;
+        this.status = status;
+    }
+
+    addTask (projectObject) {
+        projectObject.tasks.push(this)
+    }
+
+    updateTitle(newTitle) {
+        this.title = newTitle
+    }
+
+    updateDescription (newDescription) {
+        this.description = newDescription;
+    }
+
+    updateDeadline (newDeadline) {
+        this.deadline = newDeadline;
+    }
+
+    updatePriority (newPriority) {
+        this.priority = newPriority;
+    }
+
+    updateStatus (newStatus) {
+        this.status = newStatus;
+    }
+}
 
 export function createTask(title, description, deadline, priority, status) {
     return {title, description, deadline, priority, status}
 }
 
-const task1 = createTask("Read Book", "Description of Book to be read", "2015-02-01", "high", "completed");
+const task1 = new Task ("Read Book", "Description of Book to be read", "2015-02-01", "high", "completed");
 
 
 export const addTask = (taskObject, projectObject) => {
     projectObject.tasks.push(taskObject)
 }
 
-addTask(task1, projectsArray[0]);
+task1.addTask(projectsArray[0])
 
 
 console.log(projectsArray)
