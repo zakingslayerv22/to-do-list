9aab6c404852f41c7d75c64e3d942bf0270c7913
2 Sam 22:38-43 <11926638+zakingslayerv22@users.noreply.github.com>
2024-09-02 13:12:59 +0200

    Make imports
    Import projectsArray,
    Project and Task.
    
    Create a new project
    and a new task.
    
    Add the project to
    the projects array
    and add the task to
    the first project.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./index.css";
+
+import { projectsArray, Project } from "./projects-module/project.module";
+import { Task } from "./tasks-module/tasks.module";
+
+
+const project2 = new Project("Project2", "green", [{title: "Learn baking", description: "Learn how to bake bread and biscuits", deadline: "2025-02-09", priority: "medium", status: "completed"}]);
+const readBook = new Task ("Read Book", "Description of Book to be read", "2015-02-01", "high", "completed");
+
+
+project2.add();
+readBook.add(projectsArray[0])
